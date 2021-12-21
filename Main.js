c7d1c5044e6a550d0b8602f3ea3f49dd3c005aeb
Tirthankar2001 <83361489+Tirthankar2001@users.noreply.github.com>
2021-12-21 23:23:58 +0530

    commit 1 to the meal generator

---
+++ Main.js
@@ -0,0 +1,56 @@
+
+
+const meal_button=document.getElementById('mealButton');
+const meal_container=document.getElementById('meal');
+
+
+meal_button.addEventListener('click',()=>{fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+.then(res => res.json())
+.then(res => {
+createMeal(res.meals[0]);});
+});
+const createMeal=(meal)=>
+{
+    const ingredients=[];
+    for(i=1;i<=20;i++)
+    {
+        if(meal[`strIngredient${i}`]) {
+			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+		}
+        else
+        {
+            break;
+        }
+
+    }
+    const newInnerHTML = `
+		<div class="row">
+			<div class="columns five">
+				<img src="${meal.strMealThumb}" alt="Meal Image">
+				${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+				${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+				${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+				<h4>Ingredients:</h4>
+				<ul>
+					${ingredients.map(ingredients => `<li>${ingredients}</li>`).join('')}
+				</ul>
+			</div>
+			<div class="columns seven">
+				<h4>${meal.strMeal}</h4>
+				<p>${meal.strInstructions}</p>
+			</div>
+		</div>
+		${meal.strYoutube ? `
+		<div class="row">
+			<h5>Video Recipe</h5>
+			<div class="videoWrapper">
+				<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+				</iframe>
+			</div>
+		</div>` : ''}
+	`;
+	
+	meal_container.innerHTML = newInnerHTML;
+}
+
